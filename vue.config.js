6326c9379d9f70815e705dea0b808b14501a390e
fayzaarr <nurfayza.rr@gmail.com>
2025-05-15 23:56:50 +0700

    new commit again

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/portfolio-vue-rfs/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/vueportfolio/' : '/',
   outputDir: 'docs',
 
   chainWebpack: config => {
     config.plugin('html').tap(args => {
-      args[0].title = "RFSYNZ Portfolio"; // Ganti sesuai nama yang diinginkan
+      args[0].title = "RFSYNZ Portfolio"; 
       return args;
     });
   }
