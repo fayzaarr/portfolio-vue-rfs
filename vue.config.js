2492c724084d913422bde251016d79e1e27138ba
fayzaarr <nurfayza.rr@gmail.com>
2025-05-16 00:09:03 +0700

    fix: update commit again

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/vueportfolio/' : '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vueportfolio/' 
+    : '/',
   outputDir: 'docs',
 
   chainWebpack: config => {
     config.plugin('html').tap(args => {
-      args[0].title = "RFSYNZ Portfolio"; 
+      args[0].title = "RFSYNZ Portfolio"
       return args;
     });
   }
